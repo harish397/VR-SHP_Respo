780274ee1642403a74601782426bc41f0a8fd442
Endlarajesh <rajeshendla48768@gmail.com>
2022-12-23 12:40:30 +0530

    4th

--- Sales/src/app/Dis/dis/dis.module.ts
+++ Sales/src/app/Dis/dis/dis.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { DisRoutingModule } from './dis-routing.module';
 import { DistLoginComponent } from './dist-login/dist-login.component';
+import { MyProfileComponent } from './my-profile/my-profile.component';
 
 
 @NgModule({
   declarations: [
-    DistLoginComponent
+    DistLoginComponent,
+    MyProfileComponent
   ],
   imports: [
     CommonModule,
