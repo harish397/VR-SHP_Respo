1242b8b9412c9713de6361238f29953787f4ffc2
Endlarajesh <rajeshendla48768@gmail.com>
2022-12-23 12:25:59 +0530

    2Nd attempt

---
+++ Sales/src/app/Dis/dis/dis.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DisRoutingModule } from './dis-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    DisRoutingModule
+  ]
+})
+export class DisModule { }
