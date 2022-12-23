f5a60fd0c2f595a6202c58ab95d1258e2ebaa425
harish397 <harishguggila7@gmail.com>
2022-12-23 13:24:13 +0530

    1st

--- Sales/src/app/Dis/dis/dis.module.ts
+++ Sales/src/app/Dis/dis/dis.module.ts
@@ -5,7 +5,6 @@ import { DisRoutingModule } from './dis-routing.module';
 import { DistLoginComponent } from './dist-login/dist-login.component';
 import { MyProfileComponent } from './my-profile/my-profile.component';
 import { PurchaseOrderComponent } from './purchase-order/purchase-order.component';
-import { ProductComponent } from './product/product.component';
 import { StockComponent } from './stock/stock.component';
 
 
@@ -14,7 +13,7 @@ import { StockComponent } from './stock/stock.component';
     DistLoginComponent,
     MyProfileComponent,
     PurchaseOrderComponent,
-    ProductComponent,
+   
     StockComponent
   ],
   imports: [
