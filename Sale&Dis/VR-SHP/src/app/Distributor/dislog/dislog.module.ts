0f5a6fb8dc4b99130287019c265d10607ab8f716
Siraj6162 <kancharlashivakumar6162@gmail.com>
2022-12-23 10:29:30 +0530

    initial commit

---
+++ Sale&Dis/VR-SHP/src/app/Distributor/dislog/dislog.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DislogRoutingModule } from './dislog-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    DislogRoutingModule
+  ]
+})
+export class DislogModule { }
