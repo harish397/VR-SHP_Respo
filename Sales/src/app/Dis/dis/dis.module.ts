2e2f8fa1faee027ff235ad98438dc00c7f91efdf
Siraj6162 <kancharlashivakumar6162@gmail.com>
2022-12-23 13:20:36 +0530

    5

--- Sales/src/app/Dis/dis/dis.module.ts
+++ Sales/src/app/Dis/dis/dis.module.ts
@@ -5,13 +5,17 @@ import { DisRoutingModule } from './dis-routing.module';
 import { DistLoginComponent } from './dist-login/dist-login.component';
 import { MyProfileComponent } from './my-profile/my-profile.component';
 import { PurchaseOrderComponent } from './purchase-order/purchase-order.component';
+import { ProductComponent } from './product/product.component';
+import { StockComponent } from './stock/stock.component';
 
 
 @NgModule({
   declarations: [
     DistLoginComponent,
     MyProfileComponent,
-    PurchaseOrderComponent
+    PurchaseOrderComponent,
+    ProductComponent,
+    StockComponent
   ],
   imports: [
     CommonModule,
