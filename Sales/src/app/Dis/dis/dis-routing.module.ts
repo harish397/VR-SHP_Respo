e82c66a4e3d1241955091c8c3deb608d13ffff1e
harish397 <harishguggila7@gmail.com>
2022-12-24 22:03:07 +0530

    Home commit

--- Sales/src/app/Dis/dis/dis-routing.module.ts
+++ Sales/src/app/Dis/dis/dis-routing.module.ts
@@ -4,19 +4,14 @@ import { DistLoginComponent } from './dist-login/dist-login.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  {
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-    path:"",component:DistLoginComponent
-=======
-    path:"b",component:DistLoginComponent
-  },
+
   {
     path:"",component:HomeComponent
->>>>>>> Stashed changes
-=======
+  },
+  {
+
     path:"Login",component:DistLoginComponent
->>>>>>> Stashed changes
+
   }
   
 ];
