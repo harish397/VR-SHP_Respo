cb0c8b83a7fe57f6a1ee43ec88edff9e0995f0fb
harish397 <harishguggila7@gmail.com>
2022-12-28 11:07:21 +0530

    service

--- Sales/src/app/reg.service.ts
+++ Sales/src/app/reg.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -6,9 +6,15 @@ import { Injectable } from '@angular/core';
 })
 export class RegService {
 
+  httpOption={
+    headers:new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  }
   constructor(private http:HttpClient) { }
+
   getData(Id:any)
   {
-  this.http.get('https://localhost:44349/api/Sales/getbyId/' +Id);
+  this.http.get('https://localhost:44349/api/Sales/getbyId/' +Id,this.httpOption).subscribe();
   }
 }
